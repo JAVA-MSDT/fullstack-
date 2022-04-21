a725df9341794396e1a9bebe1660e04c037829ab
Ahmed Samy <serenitydiver@hotmail.com>
2022-04-21 14:50:44 +0200

    Done adding RepositoryRestResource annotation on ContactsRepository

--- Springboot_React/server/src/main/java/com/msdt/server/repository/ContactRepository.java
+++ Springboot_React/server/src/main/java/com/msdt/server/repository/ContactRepository.java
@@ -3,9 +3,11 @@ package com.msdt.server.repository;
 import com.msdt.server.model.Contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
+@RepositoryRestResource
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
 }
